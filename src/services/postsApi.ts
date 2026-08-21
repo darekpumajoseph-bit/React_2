f145f9cc4e46d6a2d7c81b7a9db45c2f216b27f2
danielacuervo1030-eng <danielacuervo1030@gmail.com>
2026-08-21 07:20:13 -0500

    Add files via upload

--- src/services/postsApi.ts
+++ src/services/postsApi.ts
@@ -1,1 +1,12 @@
+import axios from 'axios';
+import type { Post } from '../store/postsSlice';
 
+const API_URL = 'https://jsonplaceholder.typicode.com/posts?_limit=8';
+
+export const obtenerPosts = async (): Promise<Post[]> => {
+  const response = await axios.get<Post[]>(API_URL, {
+    timeout: 10000,
+  });
+
+  return response.data;
+};
